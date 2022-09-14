9a45dff53766816fc4604bbe49a3c06186f6baef
JulianRenaud <midway6220@gmail.com>
2022-09-14 09:26:01 -0500

    Finished exercise 2

--- OperatorExercise1/Program.cs
+++ OperatorExercise1/Program.cs
@@ -4,3 +4,17 @@ var answer = a / b;
 var remainder = a % b;
 
 Console.WriteLine($"{a} / {b} is {answer} with a remainder of {remainder}");
+
+Console.WriteLine("Please enter the radius of your circle: ");
+var radius = double.Parse(Console.ReadLine());
+
+
+
+static double AreaOfCircle(double radius)
+{
+    var area = Math.PI * Math.Pow(radius, 2);
+
+    return area;
+}
+
+Console.WriteLine(AreaOfCircle(radius));
